c46f41b025cd3f2734edc23524a622957b6d2b3e
Michael Barilla <michael@eeproto.com>
2022-07-31 21:26:35 -0400

    move user transactions into user_transactions widget and pass into transaction_list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,30 +3,15 @@ import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionList extends StatefulWidget {
-  @override
-  State<TransactionList> createState() => _TransactionListState();
-}
+class TransactionList extends StatelessWidget {
+  final List<Transaction> userTransactions;
 
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
+  TransactionList(this.userTransactions);
+  
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: _userTransactions.map((transaction) {
+      children: userTransactions.map((transaction) {
         return Card(
           child: Row(
             children: [
--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './new_transaction.dart';
 import './transaction_list.dart';
+import '../models/transaction.dart';
 
 class UserTransactions extends StatefulWidget {
   @override
@@ -9,12 +10,26 @@ class UserTransactions extends StatefulWidget {
 }
 
 class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         NewTransaction(),
-        TransactionList(),
+        TransactionList(_userTransactions),
       ],
     );
   }
