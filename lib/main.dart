07ffb3035eff13246628cf3f42b77a46549ef4da
Michael Barilla <michael@eeproto.com>
2022-07-31 21:21:12 -0400

    move new tranasaction form code from main into designated widget file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/transaction_list.dart';
 
-import 'models/transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,11 +16,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [];
-  // String titleInput;
-  // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,38 +34,7 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                    ),
-                    controller: titleController,
-                    // onChanged: (textFieldValue) => titleInput = textFieldValue,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'amount',
-                    ),
-                    controller: amountController,
-                    // onChanged: (textFieldValue) => amountInput = textFieldValue,
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      print(titleController.text);
-                    },
-                    child: Text('Add Transaction'),
-                    textColor: Colors.purple,
-                  )
-                ],
-              ),
-            ),
-          ),
+          NewTransaction(),
           TransactionList(),
         ],
       ),
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
-
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
-    
+    return  Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
+              controller: titleController,
+              // onChanged: (textFieldValue) => titleInput = textFieldValue,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'amount',
+              ),
+              controller: amountController,
+              // onChanged: (textFieldValue) => amountInput = textFieldValue,
+            ),
+            FlatButton(
+              onPressed: () {
+                print(titleController.text);
+              },
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
