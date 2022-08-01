1ab4b234231e3ac3f0fbc7375fc2eecc0f6e6057
Michael Barilla <michael@eeproto.com>
2022-07-31 21:27:03 -0400

    rename variable

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,14 +4,14 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  final List<Transaction> userTransactions;
+  final List<Transaction> transactions;
 
-  TransactionList(this.userTransactions);
-  
+  TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: userTransactions.map((transaction) {
+      children: transactions.map((transaction) {
         return Card(
           child: Row(
             children: [
